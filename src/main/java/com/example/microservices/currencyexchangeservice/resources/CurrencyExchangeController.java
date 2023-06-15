31b0dd3b2c683e77218b6a71ab19279c26003daf
Daniel Campos <d.camposmartins99@gmail.com>
2023-06-15 15:49:55 -0300

    custom instrumentation

--- src/main/java/com/example/microservices/currencyexchangeservice/resources/CurrencyExchangeController.java
+++ src/main/java/com/example/microservices/currencyexchangeservice/resources/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.example.microservices.currencyexchangeservice.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.Thread;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,8 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.microservices.currencyexchangeservice.domain.CurrencyExchange;
 import com.example.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
 
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Histogram;
+
 @RestController
 public class CurrencyExchangeController {
+	
+	private final Histogram requestDuration;
+	
+	public CurrencyExchangeController(CollectorRegistry collectorRegistry) {
+		requestDuration = Histogram.build()
+				.name("request_duration")
+				.help("Time for HTTTP request")
+				.register(collectorRegistry);
+	}
 
 	@Autowired
 	private CurrencyExchangeRepository repository;
@@ -22,6 +36,7 @@ public class CurrencyExchangeController {
 	public CurrencyExchange retrieveExchangeValue(
 			@PathVariable String from,
 			@PathVariable String to) {
+		Histogram.Timer timer = requestDuration.startTimer();
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		
 		if(currencyExchange == null) {
@@ -30,11 +45,32 @@ public class CurrencyExchangeController {
 		String port = environment.getProperty("local.server.port");
 		currencyExchange.setEnvironment(port);
 		
+		timer.observeDuration();
+		
 		return currencyExchange;
 	}
 	
 	@GetMapping("/up")
 	public String up() {
+		Histogram.Timer timer = requestDuration.startTimer();
+		timer.observeDuration();
+		return "up";
+	}
+	
+	@GetMapping("/upwait")
+	public String upwait() {
+		Histogram.Timer timer = requestDuration.startTimer();
+		Integer num = 2000;
+		try{
+			Thread.sleep(Long.parseLong(num.toString()));
+			wait(Long.parseLong(num.toString()));
+		}catch(Exception e){
+			System.out.println("alou");
+		}
+		
+		
+		
+		timer.observeDuration();
 		return "up";
 	}
 }
